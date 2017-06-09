b612b86ce626a918e87f5f2ec93ea7afd7cfdf93
Robert Helms <robertjohnhelms@gmail.com>
2017-06-09 23:22:02 +0100

    Added basic SQL Server database, practiced SQL queries.

---
+++ Cookbook/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿SELECT * FROM Recipe
+
+select * from Ingredient
+
+select a.Name from Ingredient a
+inner join RecipeIngredient b on a.Id = b.IngredientId
+where b.RecipeId = 1;
+
+UPDATE Recipe
+SET name = 'Salad'
+WHERE Id = 1;
+
+insert into Recipe
+values ('Salad New', 50, 'do this then dat yah');
+
+delete from Recipe
+where Id = 3;
