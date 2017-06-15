0e1117b7406e27784e4cc5246344e5ca9c1fdc1d
Robert Helms <robertjohnhelms@gmail.com>
2017-06-15 16:32:04 +0100

    connected database to program and populated a listbox

---
+++ Cookbook/frmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Cookbook
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRecipes = new System.Windows.Forms.Label();
+            this.lstRecipes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblRecipes
+            // 
+            this.lblRecipes.AutoSize = true;
+            this.lblRecipes.Location = new System.Drawing.Point(13, 13);
+            this.lblRecipes.Name = "lblRecipes";
+            this.lblRecipes.Size = new System.Drawing.Size(59, 17);
+            this.lblRecipes.TabIndex = 0;
+            this.lblRecipes.Text = "Recipes";
+            // 
+            // lstRecipes
+            // 
+            this.lstRecipes.FormattingEnabled = true;
+            this.lstRecipes.ItemHeight = 16;
+            this.lstRecipes.Location = new System.Drawing.Point(16, 44);
+            this.lstRecipes.Name = "lstRecipes";
+            this.lstRecipes.Size = new System.Drawing.Size(117, 196);
+            this.lstRecipes.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 499);
+            this.Controls.Add(this.lstRecipes);
+            this.Controls.Add(this.lblRecipes);
+            this.Name = "frmMain";
+            this.Text = "Cookbook";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRecipes;
+        private System.Windows.Forms.ListBox lstRecipes;
+    }
+}
+
---
+++ Cookbook/frmMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Cookbook
+{
+    public partial class frmMain : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            //had to add a reference to system.configuration (project->add referenence, in top menu)
+            //this string blow contains data about the database used
+            connectionString = ConfigurationManager.ConnectionStrings["Cookbook.Properties.Settings.CookbookConnectionString"].ConnectionString;
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            PopulateRecipes();
+        }
+
+        private void PopulateRecipes()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
+            {
+                DataTable recipeTable = new DataTable();
+                adapter.Fill(recipeTable);
+
+                lstRecipes.DisplayMember = "Name";
+                lstRecipes.ValueMember = "Id";
+                lstRecipes.DataSource = recipeTable;
+            }
+        }
+    }
+}
